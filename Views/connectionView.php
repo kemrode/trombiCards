9e1959f2efd492517fe1fdf6c4cad22f7ea24825
Kemrode <kevinfichou@gmail.com>
2021-05-22 18:14:29 +0200

    <feat>newOrganization: add extends files for twig

--- Views/connectionView.php
+++ Views/connectionView.php
@@ -10,7 +10,7 @@ require '../Controllers/loginController.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- put the links about material design here and about scss files -->
     <link rel="stylesheet" href="../public/Assets/sass/connectionStylus.scss">
-    <link rel="stylesheet" href="../public/Assets/sass/main.css">
+    <link rel="stylesheet" href="../public/Assets/css/main.css">
     <title>Connexion Mon Admin</title>
 </head>
 <body>
