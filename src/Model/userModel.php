cb9690a5be6c92781b06c8f45caf3e12b7f6a577
Kemrode <kevinfichou@gmail.com>
2021-05-24 21:57:21 +0200

    <feat>newOrganization: refactoring userController and userModel

--- src/Model/userModel.php
+++ src/Model/userModel.php
@@ -76,50 +76,5 @@ class userModel
         $this->pwd = $pwd;
     }
 
-    //function to POST new user
-    public function newUserPost(\PDO $bdd){
-        try {
-            $sql = 'INSERT INTO users(Username, Userprenom, Userpseudo, Userpwd) VALUES(:userName,:userPrenom,:userPseudo;:userPwd)';
-            $request = $bdd->prepare($sql);
-            $request->execute([
-                'Username'=> $this->getName(),
-                'Userprenom' => $this->getSurname(),
-                'Userpseudo' => $this->getPseudo(),
-                'Userpwd' => $this->getPwd()
-            ]);
-            return "ok";
-        } catch (\Exception $e){
-            return $e->getMessage();
-        }
-    }
-
-    //function to fetch user infos
-    public function fetchUser(\PDO $bdd){
-        try {
-            $userConnected = $this->getName();
-            $sql = 'SELECT * FROM users WHERE Username:=userName';
-            $request = $bdd->prepare($sql);
-            $request->setFetchMode(\PDO::FETCH_CLASS, 'src\Model\userModel');
-            $request->execute(['userName'=>$userConnected]);
-            return $request->fetch();
-        } catch (\Exception $e){
-            return $e->getMessage();
-        }
-    }
-
-    //function about login of a user
-    public function login(\PDO $bdd){
-        $nameLog = htmlentities($this->getName());
-        $passwdLog = htmlentities($this->getPwd());
-        try {
-            $sql = 'SELECT Username, Userpwd FROM users WHERE Username=:nameLog AND Userpwd=:passwdLog';
-            $request = $bdd->prepare($sql);
-            $request->setFetchMode(\PDO::FETCH_CLASS, 'src\Model\userModel');
-            $request->execute(['nameLog'=>$nameLog, 'passwdLog'=>$passwdLog]);
-            return $request->fetch();
-        } catch (\Exception $e){
-            return $e->getMessage();
-        }
-    }
 
 }
