e69698bcd026b7c019eb03f2a4e2f6b048b00a4f
Kemrode <kevinfichou@gmail.com>
2021-06-10 11:12:54 +0200

    feat(master): notifications views responsiv

--- var/cache/89/8922c17c328abcc205d8b960ea196d078e38097d23d5d25df9977e68353b9717.php
+++ var/cache/89/8922c17c328abcc205d8b960ea196d078e38097d23d5d25df9977e68353b9717.php
@@ -68,35 +68,37 @@ class __TwigTemplate_ea82f5c0fd0507f7dcb5222ff1d24e6e9c4748e00a0d94c37265fc4d164
         $macros = $this->macros;
         // line 9
         echo "    <div class=\"notifContainer\">
-        <div class=\"pageTitleBox\">
-            <div class=\"labelTitle\">
-                <h2>Modifier votre notification</h2>
+        <div class=\"notifUpdate\">
+            <div class=\"pageTitleBox\">
+                <div class=\"labelTitle\">
+                    <h2>Modifier votre notification</h2>
+                </div>
             </div>
-        </div>
-        <form method=\"post\" action=\"/?controller=listNews&action=updateNotifView&param=";
-        // line 15
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifId", [], "any", false, false, false, 15), "html", null, true);
+            <form method=\"post\" action=\"/?controller=listNews&action=updateNotifView&param=";
+        // line 16
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifId", [], "any", false, false, false, 16), "html", null, true);
         echo "\" name=\"newNotifForm\">
-            <div class=\"newNotifBox newNotifBox__backgroundcolor--backgroundcolor newNotifBox__zindex--index\">
-                <div class=\"notifTitle notifTitle__zindex--index\">
-                    <input type=\"text\" name=\"notifTitle\" value=\"";
-        // line 18
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifTitle", [], "any", false, false, false, 18), "html", null, true);
+                <div class=\"newNotifBox newNotifBox__backgroundcolor--backgroundcolor newNotifBox__zindex--index\">
+                    <div class=\"notifTitle notifTitle__zindex--index\">
+                        <input type=\"text\" name=\"notifTitle\" value=\"";
+        // line 19
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifTitle", [], "any", false, false, false, 19), "html", null, true);
         echo "\">
-                </div>
-                <div class=\"notifText notifText__zindex--index\">
-                    <textarea name=\"notifText\" >";
-        // line 21
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifTxt", [], "any", false, false, false, 21), "html", null, true);
+                    </div>
+                    <div class=\"notifText notifText__zindex--index\">
+                        <textarea name=\"notifText\" >";
+        // line 22
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["notification"] ?? null), "notifTxt", [], "any", false, false, false, 22), "html", null, true);
         echo "</textarea>
+                    </div>
                 </div>
-            </div>
-            <div class=\"btnBox\">
-                <div class=\"addNewNotifBox addButtonNotifView\">
-                    <button type=\"submit\" name=\"updateBtn\">modifier</button>
+                <div class=\"btnBox\">
+                    <div class=\"addNewNotifBox addButtonNotifView\">
+                        <button type=\"submit\" name=\"updateBtn\">modifier</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 ";
     }
@@ -113,7 +115,7 @@ class __TwigTemplate_ea82f5c0fd0507f7dcb5222ff1d24e6e9c4748e00a0d94c37265fc4d164
 
     public function getDebugInfo()
     {
-        return array (  90 => 21,  84 => 18,  78 => 15,  70 => 9,  66 => 8,  61 => 6,  57 => 5,  52 => 3,  48 => 2,  37 => 1,);
+        return array (  91 => 22,  85 => 19,  79 => 16,  70 => 9,  66 => 8,  61 => 6,  57 => 5,  52 => 3,  48 => 2,  37 => 1,);
     }
 
     public function getSourceContext()
@@ -127,26 +129,28 @@ class __TwigTemplate_ea82f5c0fd0507f7dcb5222ff1d24e6e9c4748e00a0d94c37265fc4d164
 {% endblock %}
 {% block updateNotif %}
     <div class=\"notifContainer\">
-        <div class=\"pageTitleBox\">
-            <div class=\"labelTitle\">
-                <h2>Modifier votre notification</h2>
+        <div class=\"notifUpdate\">
+            <div class=\"pageTitleBox\">
+                <div class=\"labelTitle\">
+                    <h2>Modifier votre notification</h2>
+                </div>
             </div>
+            <form method=\"post\" action=\"/?controller=listNews&action=updateNotifView&param={{ notification.notifId }}\" name=\"newNotifForm\">
+                <div class=\"newNotifBox newNotifBox__backgroundcolor--backgroundcolor newNotifBox__zindex--index\">
+                    <div class=\"notifTitle notifTitle__zindex--index\">
+                        <input type=\"text\" name=\"notifTitle\" value=\"{{ notification.notifTitle }}\">
+                    </div>
+                    <div class=\"notifText notifText__zindex--index\">
+                        <textarea name=\"notifText\" >{{ notification.notifTxt }}</textarea>
+                    </div>
+                </div>
+                <div class=\"btnBox\">
+                    <div class=\"addNewNotifBox addButtonNotifView\">
+                        <button type=\"submit\" name=\"updateBtn\">modifier</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <form method=\"post\" action=\"/?controller=listNews&action=updateNotifView&param={{ notification.notifId }}\" name=\"newNotifForm\">
-            <div class=\"newNotifBox newNotifBox__backgroundcolor--backgroundcolor newNotifBox__zindex--index\">
-                <div class=\"notifTitle notifTitle__zindex--index\">
-                    <input type=\"text\" name=\"notifTitle\" value=\"{{ notification.notifTitle }}\">
-                </div>
-                <div class=\"notifText notifText__zindex--index\">
-                    <textarea name=\"notifText\" >{{ notification.notifTxt }}</textarea>
-                </div>
-            </div>
-            <div class=\"btnBox\">
-                <div class=\"addNewNotifBox addButtonNotifView\">
-                    <button type=\"submit\" name=\"updateBtn\">modifier</button>
-                </div>
-            </div>
-        </form>
     </div>
 {% endblock %}", "notificationView\\updateNotificationView.html.twig", "C:\\Users\\2217873\\Documents\\developpementPerso\\trombiCards\\templates\\notificationView\\updateNotificationView.html.twig");
     }
